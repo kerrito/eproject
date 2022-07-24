1dce53ef28baa616a92bfcebee1b09d7cf2a71c1
kerrito <muhammadarshanirfan@gmail.com>
2022-07-25 00:46:58 +0500

    fitin
    hjlkjh

--- asset/js/main.js
+++ asset/js/main.js
@@ -38,3 +38,28 @@ window.addEventListener('scroll', function () {
         header.classList.remove("p-0")
     }
 });
+// detailed pages
+var link=window.location.href;
+
+    var url=new URL(link);
+
+    var id=url.searchParams.get("id")
+
+    for(var i=0;i<data.length;i++){
+        if(data[i].id==id){
+            document.getElementById("pagename").innerHTML=data[i].name;
+            document.getElementById("link1").href=data[i].link1;
+            document.getElementById("link2").href=data[i].link2;
+            document.getElementById("link3").href=data[i].link3;
+            document.getElementById("link4").href=data[i].link4;
+            document.getElementById("link5").href=data[i].link5;
+            document.getElementById("link6").href=data[i].link6;
+            document.getElementById("link7").href=data[i].link7;
+            document.getElementById("link8").href=data[i].link8;
+            document.getElementById("title").innerHTML=data[i].title;
+            document.getElementById("title1").innerHTML=data[i].title;
+            document.getElementById("img-1").src=data[i].img;
+            document.getElementById("desc").innerHTML=data[i].detail1;
+            document.getElementById("desc2").innerHTML=data[i].detail2;
+        }
+    }
